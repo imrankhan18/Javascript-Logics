27a8cd6ca8754b0655d50c1d536a99aa7ceb88fb
imrankhan18 <imrankhan@cedcoss.com>
2022-02-08 12:03:42 +0530

    c

--- src/jsform2.js
+++ src/jsform2.js
@@ -1,54 +1,67 @@
-let arr=[];
+let arr = [];
 
-function addProduct(){
-{
-    let pid=document.getElementById("productid").value;
-    let pname=document.getElementById("productname").value;
-    let pprice=document.getElementById("productprice") .value;
+function pushProduct() {
+  {
+    let pid = document.getElementById("productid").value;
+    let pname = document.getElementById("productname").value;
+    let pprice = document.getElementById("productprice").value;
 
+    if(check(pid, pname, pprice)){
 
-        insertData(pid, pname, pprice);
-        display();
-    }
-    
+    insert(pid, pname, pprice);
+    display();
 
+  }
+ }
 
-
-
+function insert(pid, pname, pprice) {
+  arr.push({
+    id: pid,
+    name: pname,
+    price: pprice,
+  });
 }
 
-
-function insertData(pid, pname, pprice)
-{
-    arr.push({
-        "id": pid,
-        "name": pname,
-        "price": pprice
-    });
-}
-
-function display()
-{
-    let result="";
-    
-    if(arr.length ===0 )
-    {
-       document.getElementById("output").innerHTML= "<p>No values</p>";
-    }
+function display() {
+ let result ="";
+  if (arr.lenght===0){
+    document.getElementById("output").innerHTML= "<p>No values</p>"         
+  }
     else{
         
-        for(let i=0; i<arr.length; i++)
-        {
-            result +=`<tr><td>${arr[i].id}</td><td>${arr[i].name}</td><td>${arr[i].price}</td></tr>`;
+        for(let i=0; i<arr.length; i++){
+            result+= "<tr><td>"+arr[i].id+"</td><td>"+arr[i].name+"</td><td>"+arr[i].price+"</td></tr>";
         }
-        document.getElementById("output").innerHTML=`<table>
-        <tr>
-            <th>Product ID</th>
-            <th>Product Name</th>
-            <th>Product Price</th>
-        </tr>
-        ${result}
-        </table>`;
-        
-    }
+        document.getElementById("output").innerHTML =  "<table>\
+        <tr>\
+            <th>Product Id</th>\
+            <th>Product Name</th>\
+            <th>Product Price</th>\
+        </tr>"
+        +result+
+        "</table>";
+
+
 }
+}
+function check(pid, pname, pprice)
+{
+    if(pid=="" || isNaN(pid))
+    {
+        alert("Enter correct ID");
+    }
+    else if(isNaN(pname)===false || pname==="")
+    {
+        alert("Enter valid Name");
+    }
+    else if(isNaN(pprice) || pprice==="")
+    {
+        alert("Enter valid price");
+    }
+    else{
+        return true;
+    }
+
+}
+}
+
